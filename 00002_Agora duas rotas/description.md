07c457867244c1f59a2270d5e243e07cf2e397c4
thomaz@digitalhouse.com <thomaz@digitalhouse.com>
2019-07-22 18:45:08 +0000

    Mumuki Export on 2019-07-22 18:45:08 +0000

---
+++ 00002_Agora duas rotas/description.md
@@ -0,0 +1,15 @@
+Como já sabemos, muitas vezes, quando preenchemos o formulário, o usuário usa a mesma URL duas vezes.
+
+A primeiro, por ** GET ** para ver o formulário.
+
+A segundo, por ** POST ** para enviar a informaçõa preenchida.
+
+O Laravel nos permite dividir a lógica, pois nos permite escrever duas rotas diferentes que respondem a metódos diferentes.
+
+Dado isso, pedimos a você:
+
+> 1. Crie uma rota para "/cadastro" por **GET** que retorna o texto "Cadastro"
+
+> 2. Crie uma rota para "/cadastro" pelo **POST** que retorna o texto "Bem vindo"
+
+Você pode supor que está gravando sua solução diretamente no arquivo de rotas.
---
+++ 00002_Agora duas rotas/test.php
@@ -0,0 +1,35 @@
+public function testRotaGet(): void {
+    $rotasGet = Route::$rotasGet;
+    
+    $this->assertTrue(count($rotasGet) == 1, "Você precisa definir uma rota GET");
+    
+    $inicioRota = $rotasGet[0];
+    
+    $this->assertTrue($inicioRota["route"] == "inicio" || $inicioRota["route"] == "/cadastro", "A rota /cadastro não está definida");
+    
+    $this->assertTrue($inicioRota["action"] instanceof Closure, "O segundo parâmetro da rota deve ser uma função anônima");
+  
+    $resultado = $inicioRota["action"]();
+    
+    $this->assertTrue(is_string($resultado), "O resultado da rota /cadastro deve ser uma string");
+    
+    $this->assertTrue(strtolower($resultado) === "cadastro", "A rota /cadastro deve imprimir 'Cadastro', e foi impresso '$resultado'");
+  }
+
+public function testRotaPost(): void {
+  $rotasGet = Route::$routesPost;
+  
+  $this->assertTrue(count($rotasGet) == 1, "Você precisa definir uma rota POST");
+  
+  $inicioRota = $rotasGet[0];
+  
+  $this->assertTrue($inicioRota["route"] == "inicio" || $inicioRota["route"] == "/cadastro", "A rota /cadastro por POST não está definida");
+  
+  $this->assertTrue($inicioRota["action"] instanceof Closure, "O segundo parâmetro da rota deve ser uma função anônima");
+
+  $resultado = $inicioRota["action"]();
+  
+  $this->assertTrue(is_string($resultado), "O resultado da rota /cadastro deve ser uma string");
+  
+  $this->assertTrue(strtolower($resultado) === "bem vindo", "A rota /cadastro deve imprimir 'Bem vindo', e foi impresso '$resultado'");
+}
