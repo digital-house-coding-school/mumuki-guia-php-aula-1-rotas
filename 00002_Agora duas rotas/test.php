6f7884ad6dff4723771be3306d314f73a461fd31
thomaz@digitalhouse.com <thomaz@digitalhouse.com>
2019-07-30 15:55:06 +0000

    Mumuki Export on 2019-07-30 15:55:06 +0000

--- 00002_Agora duas rotas/test.php
+++ 00002_Agora duas rotas/test.php
@@ -1,5 +1,5 @@
 public function testRotaGet(): void {
-    $rotasGet = Route::$rotasGet;
+    $rotasGet = Route::$routesGet;
     
     $this->assertTrue(count($rotasGet) == 1, "Você precisa definir uma rota GET");
     
